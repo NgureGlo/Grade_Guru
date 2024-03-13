9fee03722c3543324230e4938effc050b42f87b2
ngureglo <ngmweru@gmail.com>
2024-03-13 03:08:49 +0300

    added login logic

--- authentication/login.js
+++ authentication/login.js
@@ -3,6 +3,8 @@ const loginTab = document.getElementById("login-tab");
 const signupTab = document.getElementById("signup-tab");
 const loginForm = document.getElementById("login-form");
 const signupForm = document.getElementById("signup-form");
+const loginEmail = document.getElementById("login-email");
+const loginPassword = document.getElementById("login-password");
 
 // Add click event listeners to the tabs
 loginTab.addEventListener("click", function() {
@@ -22,3 +24,24 @@ signupTab.addEventListener("click", function() {
   loginTab.classList.remove("active");
   loginForm.classList.remove("active");
 });
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = loginEmail.value
+  const password = loginPassword.value
+  fetch('http://127.0.0.1:5000/login', {
+    method: 'POST',
+    headers: {
+      Accept: "*/*"
+    },
+    body: {
+      "email" : email,
+      "password" : password
+    }
+  }).then(res => res.json())
+  .then((body) => console.log(body))
+
+
+  alert(email);
+
+})
