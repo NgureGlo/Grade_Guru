cb3688ff6c47f2d9e6b37364ad482f4322fb8de4
ngureglo <ngmweru@gmail.com>
2024-03-17 13:30:10 +0300

    Changed Profile header name id

--- profile/profile.js
+++ profile/profile.js
@@ -1,3 +1,4 @@
+const header_name = document.getElementById('headerName')
 const user_name = document.getElementById('userName')
 const user_email = document.getElementById('userEmail')
 
@@ -6,6 +7,7 @@ const user_email = document.getElementById('userEmail')
 
 document.addEventListener('DOMContentLoaded', async () => {
     const details = await getUserDetails(uid)
+    header_name.innerText = details.name
     user_name.innerText = details.name
     user_email.innerText = details.email
 })
